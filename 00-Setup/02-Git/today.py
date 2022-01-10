f7f759c4dabac9cee85ed3db34411845450cc57a
HumbertMonnot <humbert.monnot@gmail.com>
2022-01-10 17:45:35 +0100

    Fix style issues, should get a 'Good Style' now :pray:

--- 00-Setup/02-Git/today.py
+++ 00-Setup/02-Git/today.py
@@ -1,10 +1,10 @@
-'''
-This is the module docstring
-'''
+"""A module computing buddy pair names for the day"""
 
 
 def my_name_is():
+    """Return my name"""
     return "HumbertMonnot"
 
 def my_buddy_is():
+    """Return my buddy's name"""
     return "RomainPllt"
