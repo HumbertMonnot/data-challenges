efbe348bc78ece3befc13688b495033aeca8ce21
HumbertMonnot <humbert.monnot@gmail.com>
2022-01-11 14:43:49 +0100

    6th exercise

--- 01-Python/01-Programming-Basics/06-Environment/flask_option.py
+++ 01-Python/01-Programming-Basics/06-Environment/flask_option.py
@@ -2,9 +2,13 @@
 
 import os
 
-def start():
+def start(args = None):
     """returns the right message"""
-    pass  # YOUR CODE HERE
+    cle = 'flask_env'
+    valeur = os.getenv(cle.upper())
+    if args is None and valeur is None:
+        return "Starting in production mode..."
+    return f"Starting in {valeur} mode..."
 
 if __name__ == "__main__":
     print(start())
