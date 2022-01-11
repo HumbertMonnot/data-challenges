55762ecdb4feb326b44a6c53871f7777231e458d
HumbertMonnot <humbert.monnot@gmail.com>
2022-01-11 14:13:26 +0100

    Fifth exercise

--- 01-Python/01-Programming-Basics/05-System-Parameters/calc.py
+++ 01-Python/01-Programming-Basics/05-System-Parameters/calc.py
@@ -1,12 +1,14 @@
 # pylint: disable=missing-module-docstring,missing-function-docstring,eval-used
 import sys
 
-
-def main():
+def main(args=None):
     """Implement the calculator"""
-    pass  # YOUR CODE HERE
+    if args is None:
+        args = sys.argv
+    nb1 = args[1]
+    nb2 = args[3]
+    return eval(f"{nb1}{args[2]}{nb2}")
 
 
 if __name__ == "__main__":
     print(main())
-    
