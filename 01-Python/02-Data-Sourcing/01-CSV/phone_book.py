382c5cee393cfab77d0c0592ffe27c79adc1fc64
HumbertMonnot <humbert.monnot@gmail.com>
2022-01-12 11:31:43 +0100

    First exercise

--- 01-Python/02-Data-Sourcing/01-CSV/phone_book.py
+++ 01-Python/02-Data-Sourcing/01-CSV/phone_book.py
@@ -1,1 +1,10 @@
+import csv
+
+with open('data/phone_book.csv', mode='r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    line_count = 0
+    for row in csv_reader:
+        last_name = row['last_name']
+        phone_number = row['phone_number']
+        print(f"{last_name.capitalize()} : {phone_number}") 
 
