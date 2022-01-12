30ce3f2059d8d0851211ab6b12ab70fb193a6d40
HumbertMonnot <humbert.monnot@gmail.com>
2022-01-12 14:49:09 +0100

    scraping exercise

--- 01-Python/02-Data-Sourcing/03-Scraping/recipe.py
+++ 01-Python/02-Data-Sourcing/03-Scraping/recipe.py
@@ -1,25 +1,41 @@
 # pylint: disable=missing-docstring,line-too-long
 import sys
 from os import path
-
+import csv
+from bs4 import BeautifulSoup
+import requests
 
 def parse(html):
     ''' return a list of dict {name, difficulty, prep_time} '''
-    pass  # YOUR CODE HERE
+    soup = BeautifulSoup(html, "html.parser")
+    recipes = soup.find_all("div", class_="p-2 recipe-details")
+    list_recipes =[]
+    for recipe in recipes :
+        name = recipe.find("p").string
+        elements = recipe.find("div").find_all("small")
+        prep_time = elements[0].find("span").string
+        difficulty = elements[1].find("span").string
+        list_recipes.append({"name":name,"difficulty":difficulty,"prep_time":prep_time})
+    return list_recipes
 
 def write_csv(ingredient, recipes):
     ''' dump recipes to a CSV file `recipes/INGREDIENT.csv` '''
-    pass  # YOUR CODE HERE
+    name_csv = f"recipes/{ingredient}.csv"
+    with open(name_csv, 'a', encoding="utf-8") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=recipes[0].keys())
+        writer.writeheader()
+        for recipe in recipes:
+            writer.writerow(recipe)
 
 def scrape_from_internet(ingredient, start=1):
     ''' Use `requests` to get the HTML page of search results for given ingredients. '''
-    pass  # YOUR CODE HERE
+    url = f"https://recipes.lewagon.com/?search[query]={ingredient}&button=&page={start}"
+    return requests.get(url, allow_redirects=False).content
 
 def scrape_from_file(ingredient):
     file = f"pages/{ingredient}.html"
     if path.exists(file):
-        return open(file)
-    print("Please, run the following command first:")
+        return open(file, encoding="utf-8")
     print(f'curl "https://recipes.lewagon.com/?search[query]={ingredient}" > pages/{ingredient}.html')
     sys.exit(1)
 
@@ -27,10 +43,9 @@ def scrape_from_file(ingredient):
 def main():
     if len(sys.argv) > 1:
         ingredient = sys.argv[1]
-        # TODO: Replace scrape_from_file with scrape_from_internet and implement pagination (more than 2 pages needed)
-        recipes = parse(scrape_from_file(ingredient))
-        write_csv(ingredient, recipes)
-        pass  # YOUR CODE HERE
+        for k in range(1,6):
+            recipes = parse(scrape_from_internet(ingredient,k))
+            write_csv(ingredient, recipes)
     else:
         print('Usage: python recipe.py INGREDIENT')
         sys.exit(0)
--- 01-Python/02-Data-Sourcing/03-Scraping/test_scraping.py
+++ 01-Python/02-Data-Sourcing/03-Scraping/test_scraping.py
@@ -1,4 +1,10 @@
-# pylint: disable=missing-docstring,invalid-name
+#pylint: disable=missing-docstring,invalid-name
+#pylint: disable=missing-docstring,line-too-long
 
-# TODO: paste the code from Kitt's instructions
+from bs4 import BeautifulSoup
 
+with open("pages/carrot.html", encoding="utf-8") as lien:
+    soup = BeautifulSoup(lien, "html.parser")
+
+for recipe in soup.find_all('p', class_= 'recipe-name'):
+    print(recipe.text)
