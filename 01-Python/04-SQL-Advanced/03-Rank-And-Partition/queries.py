430c9441d5eee12c136ef5f8e91c55fb21c20e5e
HumbertMonnot <humbert.monnot@gmail.com>
2022-01-14 16:08:53 +0100

    4th exercise

--- 01-Python/04-SQL-Advanced/03-Rank-And-Partition/queries.py
+++ 01-Python/04-SQL-Advanced/03-Rank-And-Partition/queries.py
@@ -1,8 +1,35 @@
 # pylint:disable=C0111,C0103
 
 def order_rank_per_customer(db):
-    pass  # YOUR CODE HERE
-
+    query = """SELECT OrderID, CustomerID, OrderDate, RANK() OVER(PARTITION BY CustomerID
+            ORDER BY OrderDate
+            ) AS OrderRank
+            FROM Orders"""
+    db.execute(query)
+    return db.fetchall()
 
 def order_cumulative_amount_per_customer(db):
-    pass  # YOUR CODE HERE
+    query = """WITH per_comm AS (
+SELECT 
+	Orders.OrderID ,
+	Orders.CustomerID,
+	Orders.OrderDate,
+	SUM(OrderDetails.UnitPrice*OrderDetails.Quantity) OVER(
+		PARTITION BY Orders.OrderID
+		) AS order_amount
+FROM Orders
+JOIN OrderDetails ON Orders.OrderID = OrderDetails.OrderID
+)
+SELECT 
+	per_comm.OrderID ,
+	per_comm.CustomerID,
+	per_comm.OrderDate,
+	SUM(order_amount) OVER(
+		PARTITION BY CustomerID
+		ORDER BY OrderDate) AS Cumul
+FROM per_comm
+GROUP BY OrderID
+ORDER BY CustomerID
+"""
+    db.execute(query)
+    return db.fetchall()
