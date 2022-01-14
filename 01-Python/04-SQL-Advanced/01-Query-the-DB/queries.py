1c13975c996ee17ba4be3f7646bf3255b277fd15
HumbertMonnot <humbert.monnot@gmail.com>
2022-01-14 11:50:04 +0100

    2nd exercise with style

--- 01-Python/04-SQL-Advanced/01-Query-the-DB/queries.py
+++ 01-Python/04-SQL-Advanced/01-Query-the-DB/queries.py
@@ -1,4 +1,4 @@
-# pylint:disable=C0111,C0103,C0305
+# pylint:disable=C0111,C0103
 
 def query_orders(db):
     # return a list of orders displaying each column
@@ -7,7 +7,6 @@ def query_orders(db):
     results = results.fetchall()
     return results
 
-
 def get_orders_range(db, date_from, date_to):
     # return a list of orders displaying all columns with OrderDate between date_from and date_to
     query = "SELECT * FROM orders WHERE orders.OrderDate > ? AND orders.OrderDate <= ?"
@@ -15,14 +14,12 @@ def get_orders_range(db, date_from, date_to):
     results = results.fetchall()
     return results
 
-
 def get_waiting_time(db):
     # get a list with all the orders displaying each column
     # and calculate an extra TimeDelta column displaying the number of days
     # between OrderDate and ShippedDate, ordered by ascending TimeDelta
-    query = "SELECT * , JULIANDAY(orders.ShippedDate)-JULIANDAY(orders.OrderDate) \
-            AS TimeDelta FROM orders ORDER BY TimeDelta ASC"
+    query = """SELECT * , JULIANDAY(orders.ShippedDate)-JULIANDAY(orders.OrderDate) \
+        AS TimeDelta FROM orders ORDER BY TimeDelta ASC"""
     results = db.execute(query)
     results = results.fetchall()
     return results
-
