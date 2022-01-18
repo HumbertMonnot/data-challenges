f2fecaa05da0f6a6f9268e2c0c9e6de015ce88cd
HumbertMonnot <humbert.monnot@gmail.com>
2022-01-18 11:34:04 +0100

    Completed warmup step

--- 02-Data-Toolkit/02-Data-Sourcing/00-Warmup/opengraph.py
+++ 02-Data-Toolkit/02-Data-Sourcing/00-Warmup/opengraph.py
@@ -9,9 +9,13 @@ def fetch_metadata(url):
     """
     Return a dictionary of OpenGraph metadata found in HTML of given url
     """
-    pass  # YOUR CODE HERE
+    url2 = f"""https://opengraph.lewagon.com/?url={url}"""
+    response = requests.get(url2)
+    if response.status_code != 200:
+        return None
+    return response.json()['data']
 
-# To manually test, please uncomment the following lines and run `python opengraph.py`:
-# import pprint
-# pp = pprint.PrettyPrinter(indent=4)
-# pp.pprint(fetch_metadata("https://www.lewagon.com"))
+#To manually test, please uncomment the following lines and run `python opengraph.py`:
+#import pprint
+#pp = pprint.PrettyPrinter(indent=4)
+#print(fetch_metadata("https://www.lewagon.com"))
